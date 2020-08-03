97689816261175b95ee4de5db2f7a831b1a1658a
Akia Vongdara <vongdarakia@gmail.com>
2020-08-02 18:13:56 -0700

    Add monospace font for node text
    Also add a css class in case we want to use it

--- src/utils/drawText.js
+++ src/utils/drawText.js
@@ -1,5 +1,7 @@
+import { FONT_FAMILY_MONOSPACE } from "./styleConfig";
+
 const drawText = (node, ctx, text, fontSize) => {
-    ctx.font = `${fontSize}px Sans-Serif`;
+    ctx.font = `${fontSize}px ${FONT_FAMILY_MONOSPACE}`;
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.fillStyle = "black";
