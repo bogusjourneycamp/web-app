8d9a947149dca58234276a89c4b0a26802a66ce9
Hanu Kunduru <hanu@openinvest.co>
2020-08-06 03:34:38 -0700

    fix: hard coding api url to dev env

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,5 @@
-import getEnvironment from "./getEnvironment";
+export const API_URL = `https://ja22kxm0gk.execute-api.us-east-1.amazonaws.com/dev`;
 
-export const API_URL = `https://ja22kxm0gk.execute-api.us-east-1.amazonaws.com/${getEnvironment()}`;
 
 const Urls = {
     API_URL,
