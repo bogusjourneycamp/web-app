dc18394785ca9eafd91108054187718f15295850
Akia Vongdara <vongdarakia@gmail.com>
2020-08-06 02:32:35 -0700

    Update ExplorerPage story fetching to use new API_URL

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,13 +1,14 @@
-import styled from 'styled-components';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Layout } from '../components/Layout';
+import styled from "styled-components";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Layout } from "../components/Layout";
+import { API_URL } from "../utils/urls";
 
 const loadingNode = {
-    title: 'test',
-    location: 'A_12:30',
-    text: 'Welcome!',
-    name: 'root',
+    title: "test",
+    location: "A_12:30",
+    text: "Welcome!",
+    name: "root",
     choices: [],
 };
 
@@ -44,27 +45,27 @@ const StyledContainer = styled.div`
     }
 
     #navfastforward {
-      grid-column: 3;
-      grid-row: 2;
+        grid-column: 3;
+        grid-row: 2;
     }
     #navrewind {
-      grid-column: 1;
-      grid-row: 2;
+        grid-column: 1;
+        grid-row: 2;
     }
     #navtowards {
-      grid-column: 2;
-      grid-row: 1;
+        grid-column: 2;
+        grid-row: 1;
     }
     #navaway {
-      grid-column: 2;
-      grid-row: 3;
+        grid-column: 2;
+        grid-row: 3;
     }
     #navcenter {
-      grid-column: 2;
-      grid-row: 2;
-      display: flex;
-      justify-content: center;
-      align-items: center;
+        grid-column: 2;
+        grid-row: 2;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 
     #story {
@@ -154,7 +155,7 @@ const StyledContainer = styled.div`
 `;
 
 function Actions(props) {
-  let value = props.value
+    let value = props.value;
     return (
         <div id="actions">
             {value.map((item, index) => (
@@ -164,59 +165,127 @@ function Actions(props) {
             ))}
         </div>
     );
-  }
+}
 
-function nextLocation(location, dir){
-  const alphabet = ["Man", "Esplanade", "A", "B", "C", "D", "E", "F", "G"]
-  const clock = ['1:00', '1:15', '1:30', '1:45', '2:00', '2:15', '2:30', '2:45', '3:00', '3:15', '3:30', '3:45', '4:00', '4:15', '4:30', '4:45', '5:00', '5:15', '5:30', '5:45', '6:00', '6:15', '6:30', '6:45', '7:00', '7:15', '7:30', '7:45', '8:00', '8:15', '8:30', '8:45', '9:00', '9:15', '9:30', '9:45', '10:00', '10:15', '10:30', '10:45', '11:00', '11:15', '11:30', '11:45', '12:00', '12:15', '12:30', '12:45']
-  let split = location.split("_")
-  let letter = split[0]
-  let time = split[1]
-  let letterInd = alphabet.indexOf(letter)
-  let clockInd = clock.indexOf(time)
-  
-  if (dir === 'towards') {
-    if (letterInd === 0) {
-      return location
+function nextLocation(location, dir) {
+    const alphabet = ["Man", "Esplanade", "A", "B", "C", "D", "E", "F", "G"];
+    const clock = [
+        "1:00",
+        "1:15",
+        "1:30",
+        "1:45",
+        "2:00",
+        "2:15",
+        "2:30",
+        "2:45",
+        "3:00",
+        "3:15",
+        "3:30",
+        "3:45",
+        "4:00",
+        "4:15",
+        "4:30",
+        "4:45",
+        "5:00",
+        "5:15",
+        "5:30",
+        "5:45",
+        "6:00",
+        "6:15",
+        "6:30",
+        "6:45",
+        "7:00",
+        "7:15",
+        "7:30",
+        "7:45",
+        "8:00",
+        "8:15",
+        "8:30",
+        "8:45",
+        "9:00",
+        "9:15",
+        "9:30",
+        "9:45",
+        "10:00",
+        "10:15",
+        "10:30",
+        "10:45",
+        "11:00",
+        "11:15",
+        "11:30",
+        "11:45",
+        "12:00",
+        "12:15",
+        "12:30",
+        "12:45",
+    ];
+    let split = location.split("_");
+    let letter = split[0];
+    let time = split[1];
+    let letterInd = alphabet.indexOf(letter);
+    let clockInd = clock.indexOf(time);
+
+    if (dir === "towards") {
+        if (letterInd === 0) {
+            return location;
+        }
+        return alphabet[letterInd - 1] + "_" + time;
+    } else if (dir === "away") {
+        if (letterInd === alphabet.length - 1) {
+            return location;
+        }
+        return alphabet[letterInd + 1] + "_" + time;
+    } else if (dir === "clockwise") {
+        if (clockInd === clock.length - 1) {
+            return letter + "_" + clock[0];
+        }
+        return letter + "_" + clock[clockInd + 1];
+    } else if (dir === "counter_clockwise") {
+        if (clockInd === 0) {
+            return letter + "_" + clock[clock.length - 1];
+        }
+        return letter + "_" + clock[clockInd - 1];
     }
-    return alphabet[letterInd - 1] + "_" + time
-  }
-  else if (dir === 'away') {
-    if (letterInd === alphabet.length - 1) {
-      return location
-    }
-    return alphabet[letterInd + 1] + "_" + time
-  }
-  else if (dir === 'clockwise') {
-    if (clockInd === clock.length - 1) {
-      return letter + "_" + clock[0]
-    }
-    return letter + "_" + clock[clockInd + 1]
-  }
-  else if (dir === 'counter_clockwise') {
-    if (clockInd === 0) {
-      return letter + "_" + clock[clock.length - 1]
-    }
-    return letter + "_" + clock[clockInd - 1]
-  }
 }
 
 function Navigation(props) {
     return (
         <div id="navigation">
-            <div id="navcenter">
-            {props.location}
-            </div>
-            <button id = "navaway" onClick={() => props.onNewLocation(nextLocation(props.location, 'away'))}>
+            <div id="navcenter">{props.location}</div>
+            <button
+                id="navaway"
+                onClick={() =>
+                    props.onNewLocation(nextLocation(props.location, "away"))
+                }
+            >
                 &#x025CB;
             </button>
-            <button id = "navfastforward" onClick={() => props.onNewLocation(nextLocation(props.location, 'clockwise'))}>
+            <button
+                id="navfastforward"
+                onClick={() =>
+                    props.onNewLocation(
+                        nextLocation(props.location, "clockwise")
+                    )
+                }
+            >
                 +:15
             </button>
-            <button id = "navtowards" onClick={() => props.onNewLocation(nextLocation(props.location, 'towards'))}>
+            <button
+                id="navtowards"
+                onClick={() =>
+                    props.onNewLocation(nextLocation(props.location, "towards"))
+                }
+            >
                 &#x025EF;
             </button>
-            <button id = "navrewind" onClick={() => props.onNewLocation(nextLocation(props.location, 'counter_clockwise'))}>
+            <button
+                id="navrewind"
+                onClick={() =>
+                    props.onNewLocation(
+                        nextLocation(props.location, "counter_clockwise")
+                    )
+                }
+            >
                 -:15
             </button>
         </div>
@@ -229,7 +298,10 @@ class Edit extends React.Component {
             // For local!
             //             <div id="fancy-button"><a id="edit-button" href="http://localhost:3000/create-story">Edit</a></div>
             <div id="fancy-button">
-                <Link id="edit-button" to={{pathname: "/create-story", test: this.state}}>
+                <Link
+                    id="edit-button"
+                    to={{ pathname: "/create-story", test: this.state }}
+                >
                     Edit
                 </Link>
             </div>
@@ -249,125 +321,128 @@ class Share extends React.Component {
 }
 
 function Story(props) {
-    return <div id="story">
-        <div id="story-top">
-            <div id="title-div"><div id="title">{props.title}</div></div>
-            <Edit />
-            <Share />
+    return (
+        <div id="story">
+            <div id="story-top">
+                <div id="title-div">
+                    <div id="title">{props.title}</div>
+                </div>
+                <Edit />
+                <Share />
+            </div>
+            <div id="story-bottom">
+                <div id="story-text">{props.text}</div>
+            </div>
+            <Actions value={props.actions} onTakeAction={props.onTakeAction} />
         </div>
-        <div id="story-bottom">
-            <div id="story-text">{props.text}</div>
-        </div>
-        <Actions value={props.actions} onTakeAction={props.onTakeAction} />
-    </div>;
+    );
 }
 
 function findNode(root, name) {
     let toVisit = [root];
     while (toVisit.length > 0) {
         let node = toVisit.pop();
-        if (node['name'] === name) {
+        if (node["name"] === name) {
             return node;
         } else {
-            toVisit = toVisit.concat(node['choices']);
+            toVisit = toVisit.concat(node["choices"]);
         }
     }
     return null;
 }
 
-function unclaimedNode(location){
-  return {
-    title: 'Open Playa',
-    location: location,
-    text: `Congratulations Explorer! You've discovered an unclaimed spot in this dusty land.
+function unclaimedNode(location) {
+    return {
+        title: "Open Playa",
+        location: location,
+        text: `Congratulations Explorer! You've discovered an unclaimed spot in this dusty land.
     Now it's time to write your own story and tell your own tale. Are you ready?`,
-    name: '',
-    choices: [],
-  };
-};
+        name: "",
+        choices: [],
+    };
+}
 
-export class ExplorePage extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = loadingNode
-    const query = new URLSearchParams(this.props.location.search)
-    this.onNewLocation = this.onNewLocation.bind(this)
-    this.onTakeAction = this.onTakeAction.bind(this)
-  }
+export class ExplorePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = loadingNode;
+        const query = new URLSearchParams(this.props.location.search);
+        this.onNewLocation = this.onNewLocation.bind(this);
+        this.onTakeAction = this.onTakeAction.bind(this);
+    }
 
-  componentDidMount() {
-    let url = `https://891y83rxbd.execute-api.us-east-1.amazonaws.com/prod/story?location=${this.state.location}`;
-    let node = {}
-    fetch(url)
-      .then(res => res.json())
-      .then((res) => {
-        if (JSON.stringify(res) === '{}') {
-          node = unclaimedNode(this.state.location)
-        }
-        else {
-          node = res
-        }
+    componentDidMount() {
+        let url = `${API_URL}/story?location=${this.state.location}`;
+        let node = {};
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => {
+                if (JSON.stringify(res) === "{}") {
+                    node = unclaimedNode(this.state.location);
+                } else {
+                    node = res;
+                }
+                this.setState({
+                    name: node["location"] || "Untitled",
+                    location: node["location"],
+                    title: node["title"] || "Untitled",
+                    text: node["text"],
+                    choices: node["choices"],
+                });
+            });
+    }
+
+    onTakeAction(action_name) {
+        let node = findNode(this.state, action_name);
         this.setState({
-          name: node["location"] || 'Untitled',
-          location: node["location"],
-          title: node["title"] || 'Untitled',
-          text: node["text"],
-          choices: node["choices"],
-        })
-      });
-  }
+            text: node["text"],
+            choices: node["choices"],
+        });
+    }
 
-  onTakeAction(action_name) {
-    let node = findNode(this.state, action_name);
-    this.setState({
-      text: node["text"],
-      choices: node["choices"],
-    })
-  }
+    onNewLocation(location) {
+        this.setState({ location: location });
+        let url = `https://891y83rxbd.execute-api.us-east-1.amazonaws.com/prod/story?location=${this.state.location}`;
+        let node = {};
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => {
+                if (JSON.stringify(res) === "{}") {
+                    node = unclaimedNode(location);
+                } else {
+                    node = res;
+                }
+                this.setState({
+                    name: node["location"] || "Untitled",
+                    location: node["location"],
+                    title: node["title"] || "Untitled",
+                    text: node["text"],
+                    choices: node["choices"],
+                });
+            });
+        this.props.history.push({
+            search:
+                "?" + new URLSearchParams({ location: location }).toString(),
+        });
+    }
 
-  onNewLocation(location) {
-    this.setState({location: location})
-    let url = `https://891y83rxbd.execute-api.us-east-1.amazonaws.com/prod/story?location=${this.state.location}`;
-    let node = {}
-    fetch(url)
-      .then(res => res.json())
-      .then((res) => {
-        if (JSON.stringify(res) === '{}') {
-          node = unclaimedNode(location)
-        }
-        else {
-          node = res
-        }
-        this.setState({
-          name: node["location"] || 'Untitled',
-          location: node["location"],
-          title: node["title"] || 'Untitled',
-          text: node["text"],
-          choices: node["choices"],
-        })
-      });
-    this.props.history.push({
-      search: "?" + new URLSearchParams({location: location}).toString()
-    });
-  }
-
-  render() {
-    return (
-        <Layout>
-            <StyledContainer>
-                <Story
-                    title={this.state.title}
-                    text={this.state.text}
-                    actions={this.state.choices}
-                    onTakeAction={this.onTakeAction}
-                />
-                <Navigation
-                    location={this.state.location}
-                    getNode={this.getNode}
-                    onNewLocation={this.onNewLocation}
-                />
-            </StyledContainer>
-        </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <StyledContainer>
+                    <Story
+                        title={this.state.title}
+                        text={this.state.text}
+                        actions={this.state.choices}
+                        onTakeAction={this.onTakeAction}
+                    />
+                    <Navigation
+                        location={this.state.location}
+                        getNode={this.getNode}
+                        onNewLocation={this.onNewLocation}
+                    />
+                </StyledContainer>
+            </Layout>
+        );
+    }
 }
