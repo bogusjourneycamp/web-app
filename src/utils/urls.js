871ebe752c9a4f0bcded090c83ff72fc29978588
Akia Vongdara <vongdarakia@gmail.com>
2020-08-07 01:44:19 -0700

    Add CORS_PROXY_URL

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,8 +1,9 @@
 export const API_URL = `https://ja22kxm0gk.execute-api.us-east-1.amazonaws.com/dev`;
-
+export const CORS_PROXY_URL = "https://cors-anywhere.herokuapp.com";
 
 const Urls = {
     API_URL,
+    CORS_PROXY_URL,
 };
 
 export default Urls;
