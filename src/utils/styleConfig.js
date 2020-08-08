6a91f626db535010ef55afefd4c7b9db0b6cc7ca
Akia Vongdara <vongdarakia@gmail.com>
2020-08-08 16:24:53 -0700

    Add FONT_FAMILY_MAIN to styleConfig

--- src/utils/styleConfig.js
+++ src/utils/styleConfig.js
@@ -1,7 +1,9 @@
 export const FONT_FAMILY_MONOSPACE = "monospace";
+export const FONT_FAMILY_MAIN = "Helvetica, sans-serif";
 
 const styleConfig = {
     FONT_FAMILY_MONOSPACE,
+    FONT_FAMILY_MAIN,
 };
 
 export default styleConfig;
