d7c674be87b7b7f8a68196bef54a3f4a76d89d72
Akia Vongdara <vongdarakia@gmail.com>
2020-08-08 16:26:24 -0700

    Add getUnclaimedNode util

---
+++ src/utils/getUnclaimedNode.js
@@ -0,0 +1,12 @@
+const getUnclaimedNode = (location) => {
+    return {
+        selectionText: "Open Playa",
+        location,
+        storyText: `Congratulations Explorer! You've discovered an unclaimed spot in this dusty land.
+    Now it's time to write your own story and tell your own tale. Are you ready?`,
+        name: "",
+        choices: [],
+    };
+};
+
+export default getUnclaimedNode;
