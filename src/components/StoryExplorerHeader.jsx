fbcf8a3cc12fce3ddd23b7e08a6309ead455e5e9
Akia Vongdara <vongdarakia@gmail.com>
2020-08-08 17:42:27 -0700

    Add StoryExplorerHeader component

---
+++ src/components/StoryExplorerHeader.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import LinkButton from "./LinkButton";
+
+const Header = styled.header`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    padding-top: 10px;
+`;
+
+const Title = styled.h1`
+    display: flex;
+    flex: 1;
+    font-size: 42px;
+    font-weight: 200;
+    margin: 20px;
+    letter-spacing: 1px;
+`;
+
+const ButtonsContainer = styled.div`
+    display: flex;
+    margin-right: 20px;
+
+    #btn-share {
+        margin-left: 20px;
+    }
+`;
+
+const StoryExplorerHeader = ({ title, location }) => (
+    <Header>
+        <Title>{title}</Title>
+        <ButtonsContainer>
+            <LinkButton
+                id="btn-edit"
+                to={{
+                    pathname: "/create-story",
+                    search: location
+                        ? new URLSearchParams({ location }).toString()
+                        : undefined,
+                }}
+            >
+                Edit
+            </LinkButton>
+            {/* TODO: Link to share */}
+            <LinkButton id="btn-share">Share</LinkButton>
+        </ButtonsContainer>
+    </Header>
+);
+
+export default StoryExplorerHeader;
