3ab2f4614342ec6625de26899bf7af5f86a60536
Akia Vongdara <vongdarakia@gmail.com>
2020-08-08 17:35:23 -0700

    Add FONT_COLOR_MAIN to styleConfig

--- src/utils/styleConfig.js
+++ src/utils/styleConfig.js
@@ -1,9 +1,13 @@
+import ColorPalette from "./colors";
+
 export const FONT_FAMILY_MONOSPACE = "monospace";
 export const FONT_FAMILY_MAIN = "Helvetica, sans-serif";
+export const FONT_COLOR_MAIN = ColorPalette.spaceBlack;
 
 const styleConfig = {
     FONT_FAMILY_MONOSPACE,
     FONT_FAMILY_MAIN,
+    FONT_COLOR_MAIN,
 };
 
 export default styleConfig;
