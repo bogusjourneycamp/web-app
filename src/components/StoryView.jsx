316c147b98315abd3625125c83f10eda23f284de
ariella-infiswift <ariella@infiswift.tech>
2020-08-21 10:14:03 -0700

    Program is now able to store the value of the parent node and pass it to the storyExplorerHeader page

--- src/components/StoryView.jsx
+++ src/components/StoryView.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import StoryExplorerHeader from "./StoryExplorerHeader";
 import StoryExplorerTextView from "./StoryExplorerTextView";
 import storyNodeToGraphData from "../utils/storyNodeToGraphData"
+import getNodeById from "../utils/getNodeById"
 
 const StyledContainer = styled.div`
     width: 100%;
@@ -14,16 +15,32 @@ const StyledContainer = styled.div`
 const StoryView = ({ storyNode, onTakeAction }) => {
     const { selectionText, location, storyText, choices } = storyNode;
     let editButtonTitle = "Edit";
+	const [storyPath, setStoryPath] = useState(0);
+	const [isStoryPathSet, setIsStoryPathSet] = useState(0);
+	let previousNode = null;
+	const [rootNode, setRootNode] = useState(0);
 
     if (selectionText === "Open Playa") {
-        editButtonTitle = "Create";
+		editButtonTitle = "Create";
+		setStoryPath(0);
+		setIsStoryPathSet(1);
 	}
-	if storyNode
-	const thing = storyNodeToGraphData(storyNode, storyNode.id)
-	console.log("i ",thing )
-    return (
+	if (storyNode.name === "root" && isStoryPathSet === 0) {
+		setStoryPath(storyNodeToGraphData(storyNode, storyNode.id));
+		setIsStoryPathSet(1);
+		setRootNode(storyNode);
+	}
+	if (storyPath) {
+		for (let i = 0; i < storyPath.nodes.length; i++) {
+			if (storyPath.nodes[i].id == storyNode.id) {
+				previousNode = getNodeById(rootNode, storyPath.nodes[i].parentNodeId);
+			}
+		}
+	}
+	console.log(previousNode);
+	return (
         <StyledContainer>
-            <StoryExplorerHeader title={selectionText} location={location} editButtonTitle={editButtonTitle} />
+            <StoryExplorerHeader title={selectionText} location={location} editButtonTitle={editButtonTitle} previousNode={previousNode}/>
             <StoryExplorerTextView
                 storyText={storyText}
                 onTakeAction={onTakeAction}
