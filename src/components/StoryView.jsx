5674923692bd54cc8a82a8ecdbd8a1e3bdd8d954
blairclair <ariellagrodzinsky@gmail.com>
2020-08-20 17:56:06 -0700

    Found path from the root node to the current node

--- src/components/StoryView.jsx
+++ src/components/StoryView.jsx
@@ -2,38 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import StoryExplorerHeader from "./StoryExplorerHeader";
 import StoryExplorerTextView from "./StoryExplorerTextView";
-import getNodePath from "../utils/getNodePath";
-import {previousLocation} from '../utils/getNextLocation';
+import storyNodeToGraphData from "../utils/storyNodeToGraphData"
 
 const StyledContainer = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
 `;
-const GetPreviousLocation = ({storyNode}) => {
-    // const nodePath = getNodePath(storyNode);
 
-    // if (!nodePath) {
-    //     return (null);
-    // }
-    // if (nodePath && nodePath === 1) {
-    //     return (null);
-    // }
-    // const previousLocation = nodePath[nodePath.length - 2].location;
-    return ("gdf");
-};
 
 const StoryView = ({ storyNode, onTakeAction }) => {
     const { selectionText, location, storyText, choices } = storyNode;
     let editButtonTitle = "Edit";
+
     if (selectionText === "Open Playa") {
         editButtonTitle = "Create";
-    }
-	console.log(previousLocation)
-    
+	}
+	if storyNode
+	const thing = storyNodeToGraphData(storyNode, storyNode.id)
+	console.log("i ",thing )
     return (
         <StyledContainer>
-            <StoryExplorerHeader title={selectionText} location={location} editButtonTitle={editButtonTitle} previousLocation={previousLocation} />
+            <StoryExplorerHeader title={selectionText} location={location} editButtonTitle={editButtonTitle} />
             <StoryExplorerTextView
                 storyText={storyText}
                 onTakeAction={onTakeAction}
