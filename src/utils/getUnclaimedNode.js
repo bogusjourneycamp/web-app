783280e9e8f2fab06e64eb6f111f8dfcf986ab66
Akia Vongdara <vongdarakia@gmail.com>
2020-08-22 17:27:30 -0700

    Add newline to unclaimed node default story text
    Because the second sentence shows up with a weird indentation

--- src/utils/getUnclaimedNode.js
+++ src/utils/getUnclaimedNode.js
@@ -2,8 +2,7 @@ const getUnclaimedNode = (location) => {
     return {
         selectionText: "Open Playa",
         location,
-        storyText: `Congratulations Explorer! You've discovered an unclaimed spot in this dusty land.
-    Now it's time to write your own story and tell your own tale. Are you ready?`,
+        storyText: `Congratulations Explorer! You've discovered an unclaimed spot in this dusty land.\n\nNow it's time to write your own story and tell your own tale. Are you ready?`,
         name: "",
         choices: [],
     };
