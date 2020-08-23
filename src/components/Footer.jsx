7121f29f4d389f3c80be61f83407299ae8bcbd55
Akia Vongdara <vongdarakia@gmail.com>
2020-08-22 17:02:20 -0700

    Add photo credit to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyledFooter = styled.footer`
     padding: 16px;
     text-align: center;
+    position: relative;
+`;
+
+const PhotoCredit = styled.span`
+    position: absolute;
+    right: 10px;
 `;
 
 export const Footer = () => {
-    return <StyledFooter>The Lost Arts &copy; 2020</StyledFooter>;
+    return (
+        <StyledFooter>
+            The Lost Arts &copy; 2020
+            <PhotoCredit>Photo by Mitchell aka Argus</PhotoCredit>
+        </StyledFooter>
+    );
 };
