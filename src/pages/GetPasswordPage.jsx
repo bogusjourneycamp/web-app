f517453451c56caaf01937bc0f86bfd709375be9
Charlie Summers <charlie@gomerits.com>
2020-08-25 22:56:23 -0700

    Format GetPasswordPage

--- src/pages/GetPasswordPage.jsx
+++ src/pages/GetPasswordPage.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 import { Layout } from "../components/Layout";
 import StoryButton from "../components/StoryButton";
+import StoryFrame from "../components/StoryFrame";
+import ColorPalette from "../utils/colors";
+
+const StyledContainer = styled.div`
+    padding: 12px;
+    display: flex;
+    flex-direction: column;
+`;
+
+const TitleContainer = styled.div`
+    font-size: 16px;
+`;
+
+const PassphraseContainer = styled.div`
+    padding: 12px;
+    font-size: 20px;
+`;
+
+const ExtraCopyContainer = styled.div`
+    font-size: 16px;
+`;
 
 const GetPasswordPage = ({history}) => {
     const loc = useLocation();
@@ -15,14 +37,18 @@ const GetPasswordPage = ({history}) => {
         });
         console.log("History has been pushed");
     }
-	
+
     return (
-		
         <Layout>
-            Your passphrase is:  {loc.state.password}. Write it down, you will need it for editing!
-            <StoryButton id="btn_return" onClick={onClickReturn}>To your story</StoryButton>
+            <StoryFrame>
+                <StyledContainer>
+                    <TitleContainer>Your passphrase is:</TitleContainer>
+                    <PassphraseContainer>{loc.state.password}</PassphraseContainer>
+                    <ExtraCopyContainer>Write it down. You'll need to put it in the "Passphrase for Edit" box in order to edit your story later.</ExtraCopyContainer>
+                    <StoryButton onClick={onClickReturn}>To your story!</StoryButton>
+                </StyledContainer>
+            </StoryFrame>
         </Layout>
-		
     );
 }
 export default GetPasswordPage;
