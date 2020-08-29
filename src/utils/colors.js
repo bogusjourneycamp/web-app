3972062412e4e0caae87d6140bf0b518d84b508c
Akia Vongdara <vongdarakia@gmail.com>
2020-08-29 12:55:59 -0700

    Add DisabledRed color

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,10 +1,11 @@
 const ColorPalette = {
-    CornflowerBlue: "#0275d8",
-    SpaceBlack: "#373a3c",
-    NodeGreen: "#5cb85c",
-    NodeGray: "#c4c4c4",
-    NodeRed: "#b85c5c",
     CoffeeBean: "#351212",
+    CornflowerBlue: "#0275d8",
+    DisabledRed: "rgba(219, 112, 147, 0.5)",
+    NodeGray: "#c4c4c4",
+    NodeGreen: "#5cb85c",
+    NodeRed: "#b85c5c",
+    SpaceBlack: "#373a3c",
 };
 
 export default ColorPalette;
