7f370f5ae9ff459c593e83f5e7cfeff07f1ae416
Akia Vongdara <vongdarakia@gmail.com>
2020-08-30 02:27:09 -0700

    Use Antd Button for our whole app
    This allows the usage of loading indicator

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import { Button as AntdButton } from "antd";
+import ColorPalette from "../utils/colors";
 
-const StyledButton = styled.button`
+const StyledButton = styled(AntdButton)`
     cursor: pointer;
+
+    :hover {
+        border-color: ${ColorPalette.CoffeeBean};
+    }
 `;
 
-const Button = ({ children, ...props }) => (
-    <StyledButton {...props}>{children}</StyledButton>
-);
+const Button = ({ children, ...props }) => <StyledButton {...props}>{children}</StyledButton>;
 
 export default Button;
