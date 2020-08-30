45168e658c47631e68e9e622a73483d7a1b49e4f
Akia Vongdara <vongdarakia@gmail.com>
2020-08-30 01:10:50 -0700

    Use id as testId for react testing library

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+import { configure as configureRTL } from "@testing-library/react";
 import fetchMock from "jest-fetch-mock";
 
 fetchMock.enableMocks();
+
+// Use id attribute as the testId that @testing-library looks for
+configureRTL({ testIdAttribute: "id" });
